e22e015c6cc8b12efb75d5a2f64a4ffe80c247fe
Dennis Birkholz <dennis@birkholz.org>
2018-04-01 01:16:45 +0200

    Class parsing end of central directory

---
+++ src/zip/EndOfCentralDirectory.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace iqb\zip;
+
+class EndOfCentralDirectory
+{
+    const SIGNATURE = 0x504b0506;
+
+    const MIN_LENGTH = 22;
+
+    public $numberOfThisDisk;
+
+    public $numberOfTheDiskWithTheStartOfTheCentralDirectory;
+
+    public $totalNumberOfEntriesInTheCentralDirectoryOnThisDisk;
+
+    public $totalNumberOfEntriesInTheCentralDirectory;
+
+    public $sizeOfTheCentralDirectory;
+
+    public $offsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber;
+
+    public $zipFileCommentLength;
+
+    public $zipFileComment;
+
+    /// @var int
+    public $requireAdditionalData;
+
+
+    public function __construct(
+        int $numberOfThisDisk,
+        int $numberOfTheDiskWithTheStartOfTheCentralDirectory,
+        int $totalNumberOfEntriesInTheCentralDirectoryOnThisDisk,
+        int $totalNumberOfEntriesInTheCentralDirectory,
+        int $sizeOfTheCentralDirectory,
+        int $offsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber,
+        string $zipFileComment = null
+    ) {
+        $this->numberOfThisDisk = $numberOfThisDisk;
+        $this->numberOfTheDiskWithTheStartOfTheCentralDirectory = $numberOfTheDiskWithTheStartOfTheCentralDirectory;
+        $this->totalNumberOfEntriesInTheCentralDirectoryOnThisDisk = $totalNumberOfEntriesInTheCentralDirectoryOnThisDisk;
+        $this->totalNumberOfEntriesInTheCentralDirectory = $totalNumberOfEntriesInTheCentralDirectory;
+        $this->sizeOfTheCentralDirectory = $sizeOfTheCentralDirectory;
+        $this->offsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber = $offsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber;
+
+        if ($zipFileComment !== null) {
+            $this->zipFileComment = $zipFileComment;
+            $this->zipFileCommentLength = \strlen($this->zipFileComment);
+        }
+    }
+
+
+    /**
+     * Parse the end of central directory from a binary string.
+     * Check $requireAdditionalData to check if parseAdditionalData() must be called to parse additional fields.
+     *
+     * @param string $input
+     * @param int $offset Start at this position inside the string
+     * @return static
+     */
+    public static function parse(string $input, int $offset = 0)
+    {
+        if (\strlen($input) < ($offset+self::MIN_LENGTH)) {
+            throw new \InvalidArgumentException("Not enough data to parse local file header!");
+        }
+
+        $parsed = \unpack(
+            'Nsignature'
+            . '/vnumberOfThisDisk'
+            . '/vnumberOfTheDiskWithTheStartOfTheCentralDirectory'
+            . '/vtotalNumberOfEntriesInTheCentralDirectoryOnThisDisk'
+            . '/vtotalNumberOfEntriesInTheCentralDirectory'
+            . '/VsizeOfTheCentralDirectory'
+            . '/VoffsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber'
+            . '/vzipFileCommentLength',
+            $input,
+            $offset
+        );
+        if ($parsed['signature'] !== self::SIGNATURE) {
+            throw new \InvalidArgumentException("Invalid signature for local file header!");
+        }
+
+        $endOfCentralDirectory = new static(
+            $parsed['numberOfThisDisk'],
+            $parsed['numberOfTheDiskWithTheStartOfTheCentralDirectory'],
+            $parsed['totalNumberOfEntriesInTheCentralDirectoryOnThisDisk'],
+            $parsed['totalNumberOfEntriesInTheCentralDirectory'],
+            $parsed['sizeOfTheCentralDirectory'],
+            $parsed['offsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber']
+        );
+        $endOfCentralDirectory->zipFileCommentLength = $parsed['zipFileCommentLength'];
+        $endOfCentralDirectory->requireAdditionalData = $endOfCentralDirectory->zipFileCommentLength;
+
+        return $endOfCentralDirectory;
+    }
+
+
+    /**
+     * After a new object has been created by parse(), this method must be called to initialize the zip file comment entry which has dynamic field length.
+     * The required number of bytes is written to the $requireAdditionalData attribute by parse().
+     *
+     * @param string $input
+     * @param int $offset
+     * @return int
+     */
+    public function parseAdditionalData(string $input, int $offset = 0) : int
+    {
+        if ($this->zipFileComment !== null) {
+            throw new \BadMethodCallException("Additional data already parsed!");
+        }
+
+        if (!$this->requireAdditionalData) {
+            throw new \BadMethodCallException("No additional data required!");
+        }
+
+        if (\strlen($input) < ($offset + $this->zipFileCommentLength)) {
+            throw new \InvalidArgumentException("Not enough input to parse additional data!");
+        }
+
+        $this->zipFileComment = \substr($input, $offset, $this->zipFileCommentLength);
+        $this->requireAdditionalData = null;
+
+        return $this->zipFileCommentLength;
+    }
+}
