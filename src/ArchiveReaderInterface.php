2b9ed39d00977c7f2e42a9892fe61e3391e2bb2e
Dennis Birkholz <dennis@birkholz.org>
2018-04-13 16:54:39 +0200

    Interfaces for conversion between Archive structures and binary archive streams

---
+++ src/ArchiveReaderInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * (c) 2018 Dennis Birkholz <dennis@birkholz.org>
+ *
+ * $Id$
+ * Author:    $Format:%an <%ae>, %ai$
+ * Committer: $Format:%cn <%ce>, %ci$
+ */
+
+namespace iqb;
+
+/**
+ * @author Dennis Birkholz <dennis@birkholz.org>
+ */
+interface ArchiveReaderInterface
+{
+    /**
+     * Read an archive from a stream
+     *
+     * @param resource $stream A seekable stream
+     * @param int $offset Stream position to seek to before reading the archive
+     * @return Archive
+     */
+    function read($stream, int $offset = 0) : Archive;
+}
---
+++ src/ArchiveWriterInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * (c) 2018 Dennis Birkholz <dennis@birkholz.org>
+ *
+ * $Id$
+ * Author:    $Format:%an <%ae>, %ai$
+ * Committer: $Format:%cn <%ce>, %ci$
+ */
+
+namespace iqb;
+
+/**
+ * @author Dennis Birkholz <dennis@birkholz.org>
+ */
+interface ArchiveWriterInterface
+{
+    /**
+     * Write an archive to a stream.
+     * Returns the updated Archive containing information only known after the Archive is written,
+     *  e.g. compressed size of entries.
+     *
+     * @param Archive $archive The Archive describing the archive structure
+     * @param resource $targetStream A seekable stream
+     * @return Archive The updated Archive structure
+     */
+    function write(Archive $archive, $targetStream) : Archive;
+}
